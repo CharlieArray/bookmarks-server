03865ae7caf92864ceb989a481db4ef942343ec8
Charles Ybarra <59151304+CharlieArray@users.noreply.github.com>
2021-01-21 18:07:09 -0600

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -134,10 +134,10 @@ bookRouter
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
+  ~console.error(error);
   if (NODE_ENV === "production") {
     response = { error: { message: "server error" } };
   } else {
-    ~console.error(error);
     response = { message: error.message, error };
   }
   res.status(500).json(response);
